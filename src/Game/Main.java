3094d9a8154b0af3ae2cfa4c5bc86e0711cf88c1
Christopher Rivera <christopherrivera384@gmail.com>
2024-02-11 19:38:12 -0500

    working on loading images

--- src/Game/Main.java
+++ src/Game/Main.java
@@ -1,9 +1,15 @@
 package Game;
 
+import ScreenManager.SimpleScreenManager;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class Main extends JFrame {
+
+    private static final long DEMO_TIME = 5000;
+    private Image testImage;
+    private boolean imagesLoaded;
     public static void main(String[] args) {
         DisplayMode displayMode;
 
@@ -23,5 +29,55 @@ public class Main extends JFrame {
                             DisplayMode.REFRESH_RATE_UNKNOWN
                     );
         }
+
+        Main main = new Main();
+
+        ImageIcon icon =
+                new ImageIcon("../../assets/tanks/tank_darkLarge.png");
+
+        main.run(displayMode);
+    }
+
+    public void run(DisplayMode displayMode) {
+        setBackground(Color.blue);
+        setForeground(Color.white);
+
+        SimpleScreenManager screenManager = new SimpleScreenManager();
+
+        try {
+            screenManager.setFullScreen(displayMode, this);
+            loadImages();
+            try {
+                Thread.sleep(DEMO_TIME);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        } finally {
+            screenManager.restoreScreen();
+        }
+    }
+
+    public void loadImages() {
+        testImage =
+                loadImage("../assets/tanks/tank_darkLarge.png");
+        imagesLoaded = true;
+        repaint();
+    }
+
+    private Image loadImage(String filename) {
+        return new ImageIcon(filename).getImage();
+    }
+
+
+    public void paint(Graphics pen) {
+        pen.drawString("Hello World!", 20, 50);
+
+        if(imagesLoaded) {
+            pen.drawImage(testImage, 0, 0, null);
+        }
+    }
+
+    public void drawImage(Graphics pen, Image image, int x, int y) {
+
     }
 }
